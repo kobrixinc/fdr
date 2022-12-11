2d0a5543c4bb1d6e8fba0baae6dc2a15dd5a75c7
Borislav Iordanov <borislav.iordanov@gmail.com>
2022-12-11 03:18:57 -0500

    ability to create meta triples (triples on triples)

--- src/fdr/fdr.ts
+++ src/fdr/fdr.ts
@@ -1,10 +1,15 @@
 import modelFactory  from "@rdfjs/data-model"
-import { NamedNode, Term } from "@rdfjs/types"
+import { NamedNode, Quad, Term } from "@rdfjs/types"
 
 export class make {
   static named(iri) { return modelFactory.namedNode(iri) }
   static literal(iri) { return modelFactory.literal(iri) }
-  static quad(x: NamedNode, y: NamedNode, z: Term, g?:Term) { return modelFactory.quad(x, y, z, g) }
+  static quad(x: NamedNode, y: NamedNode, z: Term, g?:Term) { 
+    return modelFactory.quad(x, y, z, g) 
+  }
+  static metaQuad(x: Quad, y: NamedNode, z: Term, g?: Term) {
+    return modelFactory.quad(x, y, z, g) 
+  }
 }
 
 export type LiteralValue = number | string | boolean
